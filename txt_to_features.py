b972be5126a0f147c2e14e42cf34a502ff356f6b
Sahasramylavaram <sahasramylavaram@gmail.com>
2025-06-11 21:39:29 +0530

    Update txt_to_features.py

--- txt_to_features.py
+++ txt_to_features.py
@@ -1,38 +1,19 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
-import pandas as pd
-
 
 def txt_features(p_resumetxt, p_jdtxt):
-    """
-    This function returns a dataframe of features 
-    extracted from a list of texts
-    :param p_resumetxt: preprocessed list of resume texts
-    :param p_jdtxt: preprocessed list of job description texts
-    :return: dataframe of features 
-    """
-    txt = p_resumetxt+p_jdtxt
-    tv = TfidfVectorizer(max_df=0.85,min_df=1,ngram_range=(1,3))
+    # Combine job description and all resumes
+    txt = [*p_resumetxt, p_jdtxt]
+
+    # Adjust min_df and max_df to allow small samples
+    tv = TfidfVectorizer(max_df=1.0, min_df=1, stop_words='english')
     tfidf_wm = tv.fit_transform(txt)
 
-    tfidf_tokens = tv.get_feature_names_out()
-    df_tfidfvect = pd.DataFrame(data = tfidf_wm.toarray(),columns = tfidf_tokens)
+    # Handle cases where tfidf_wm is too small
+    if tfidf_wm.shape[0] <= 1 or tfidf_wm.shape[1] < 2:
+        return tfidf_wm.toarray()
 
-    return df_tfidfvect
-
-def feats_reduce(feats_df):
-
-    """
-    This function returns a reduced dimensionality of a dataframe of features
-    :param feats_df: dataframe of features extracted from a list of texts
-    :return: reduced dimensionality of a dataframe of features
-    """
-    dimrec = TruncatedSVD(n_components=30, n_iter=7, random_state=42)
-    feats_red = dimrec.fit_transform(feats_df)
-    # Converting transformed vector to list
-    feats_red = feats_red.tolist()
-    # Converting list to DataFrame
-    feat_red = pd.DataFrame(feats_red)
-
-    return feat_red
-
+    # Reduce features with SVD (LSA)
+    svd_model = TruncatedSVD(n_components=min(100, tfidf_wm.shape[1] - 1))
+    feats_red = svd_model.fit_transform(tfidf_wm)
+    return feats_red
